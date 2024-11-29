3d0e28009a5b0d6188114e4be35df6fffa0d6d5f
ranjit1024 <ranjitdas2048@gmail.com>
2024-11-29 23:22:25 +0530

    adding init file

---
+++ index.js
@@ -0,0 +1,25 @@
+gsap.to("#box", {
+    duration:2,
+    delay:1,
+    x:1000,
+    rotate:180,
+    borderRadius:"50%"
+})
+
+gsap.to("#box1", {
+    delay:2.5,
+    backgroundColor:"green",
+    x:1000,
+    duration:2,
+    scale:0.9,
+    borderRadius:"50%",
+})
+
+gsap.to("#box2", {
+    delay:4.5,
+    backgroundColor:"black",
+    // x:1000,
+    duration:2,
+    scale:100,
+    opacity:0.6
+})
